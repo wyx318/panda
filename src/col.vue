d57d7a777a82c499088a80317b5d30d944c23a45
wangyuixang <651033260@qq.com>
2019-10-24 01:08:57 +0800

    基本实现 row.vue 组件 和 col.vue组件 的功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-	<div class="col" :class="[`col-${span}`]">
-		<slot></slot>
+	<div class="col" :class="span && [`col-${span}`,offset && `offset-${offset}`] "
+			 :style="{paddingLeft: gutter / 2+'px',paddingRight: gutter / 2+'px'}">
+		<div style="border: 1px solid green; height: 100px;">
+			<slot></slot>
+		</div>
 	</div>
 </template>
 <script>
@@ -9,6 +12,14 @@
     props: {
       span: {
         type: [Number, String],
+      },
+      offset: {
+        type: [Number, String]
+      }
+    },
+    data() {
+      return {
+        gutter: 0
       }
     }
   }
@@ -16,14 +27,19 @@
 <style scoped lang="scss">
 	.col {
 		height: 100px;
-		background: pink;
+		/*background: #666;*/
 		width: 50%;
-		border: 1px solid #000000;
-		
+		/*col设置占比*/
 		$class-prefix: col-;
 		@for $n from 1 through 24 {
 			&.#{$class-prefix}#{$n} {
 				width: ($n / 24 ) * 100%;
+			}
+		}
+		$class-prefix: offset-;
+		@for $n from 1 through 24 {
+			&.#{$class-prefix}#{$n} {
+				margin-left: ($n / 24 ) * 100%;
 			}
 		}
 	}
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,28 @@
 <template>
-	<div class="row">
+	<div class="row" :style="{marginLeft: -gutter / 2+'px',marginRight: -gutter / 2+'px'}">
 		<slot></slot>
 	</div>
 </template>
+<script>
+  export default {
+    name: 'pandaRow',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    created() {
+      console.log('row created');
+    },
+    mounted() {
+      console.log('row mouted');
+      console.log(this.$children);
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
 <style scoped lang="scss">
 	.row {
 		display: flex;
