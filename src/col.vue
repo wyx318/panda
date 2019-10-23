01b65806ef4cf168f6c72a922e4e315036a1020c
wangyuixang <651033260@qq.com>
2019-10-23 19:40:41 +0800

    row.vue 组件 和 col.vue组件 的雏形 完成

---
+++ src/col.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="col" :class="[`col-${span}`]">
+		<slot></slot>
+	</div>
+</template>
+<script>
+  export default {
+    name: 'pandaCol',
+    props: {
+      span: {
+        type: [Number, String],
+      }
+    }
+  }
+</script>
+<style scoped lang="scss">
+	.col {
+		height: 100px;
+		background: pink;
+		width: 50%;
+		border: 1px solid #000000;
+		
+		$class-prefix: col-;
+		@for $n from 1 through 24 {
+			&.#{$class-prefix}#{$n} {
+				width: ($n / 24 ) * 100%;
+			}
+		}
+	}
+</style>
