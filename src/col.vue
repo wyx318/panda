421c5dd8339aa2f4494f86069c33933afe30660a
wangyuixang <651033260@qq.com>
2019-10-25 01:06:20 +0800

    重构 row.vue 组件 和 col.vue组件

--- src/col.vue
+++ src/col.vue
@@ -1,9 +1,7 @@
 <template>
-	<div class="col" :class="span && [`col-${span}`,offset && `offset-${offset}`] "
-			 :style="{paddingLeft: gutter / 2+'px',paddingRight: gutter / 2+'px'}">
-		<div style="border: 1px solid green; height: 100px;">
-			<slot></slot>
-		</div>
+	<div class="col" :class="colClass"
+			 :style="colStyle">
+		<slot></slot>
 	</div>
 </template>
 <script>
@@ -19,7 +17,23 @@
     },
     data() {
       return {
-        gutter: 0
+        gutter: 0,
+      }
+    },
+    //涉及到计算 一定写到计算属性中去
+    computed: {
+      colClass: function () {
+        let {span, offset} = this
+        return [
+          span && `col-${span}`,
+          offset && `offset-${offset}`
+        ]
+      },
+      colStyle: function () {
+        return {
+          paddingLeft: this.gutter / 2 + 'px',
+          paddingRight: this.gutter / 2 + 'px',
+        }
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="row" :style="{marginLeft: -gutter / 2+'px',marginRight: -gutter / 2+'px'}">
+	<div class="row" :style="rowStyle">
 		<slot></slot>
 	</div>
 </template>
@@ -11,12 +11,15 @@
         type: [Number, String]
       }
     },
-    created() {
-      console.log('row created');
+    computed: {
+      rowStyle() {
+        let {gutter} = this;
+        return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+      }
     },
     mounted() {
-      console.log('row mouted');
-      console.log(this.$children);
+      // console.log('row mouted');
+      // console.log(this.$children);
       this.$children.forEach((vm) => {
         vm.gutter = this.gutter
       })
