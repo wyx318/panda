09a576ded86c2a6905cb296023275d6927f7ee1e
wangyuixang <651033260@qq.com>
2019-10-31 01:50:09 +0800

    添加简单的动画

--- src/sider.vue
+++ src/sider.vue
@@ -1,13 +1,43 @@
 <template>
-	<div class="sider">
-		<slot></slot>
-	</div>
+	<transition name="fade">
+		<div class="sider" v-if="visible">
+			<slot></slot>
+			<button @click="visible=false">close</button>
+		</div>
+	</transition>
+
 </template>
 <script>
   export default {
-    name:'PandaSider'
+    name: 'PandaSider',
+    data() {
+      return {
+        visible: true
+      }
+    },
+    methods: {}
   }
 </script>
 <style lang="scss" scoped>
-
+	.sider {
+		position: relative;
+		
+		> {
+			button {
+				position: absolute;
+				top: 0;
+				right: 0;
+			}
+			
+		}
+	}
+	
+	.fade-enter-active, .fade-leave-active {
+		transition: all .5s;
+	}
+	
+	.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+	{
+		margin-left: -200px;
+	}
 </style>
