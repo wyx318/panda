6a54220e4b1246decf38ca6df4dada2d24265825
wangyuixang <651033260@qq.com>
2019-11-05 18:06:50 +0800

    实现进入动画

--- src/toast.vue
+++ src/toast.vue
@@ -93,7 +93,14 @@
 	$font-size: 14px;
 	$toast-min-height: 40px;
 	$toast-bg: rgba(0, 0, 0, 0.75);
+	//动画效果
+	@keyframes fade-in {
+		0% {opacity: 0; transform: translateY(100%);}
+		100% {opacity: 1; transform: translateY(0%);}
+		}
+	
 	.toast {
+		animation: fade-in 1s;
 		font-size: $font-size;
 		line-height: 1.8;
 		min-height: $toast-min-height;
