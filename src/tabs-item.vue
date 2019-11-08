53e2bdb58393d4257724f855d25aa535766ef841
wangyuixang <651033260@qq.com>
2019-11-08 17:05:47 +0800

    实现 disabled 的 功能

--- src/tabs-item.vue
+++ src/tabs-item.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="tabs-item" @click="xxx" :class="classes">
+	<div class="tabs-item" @click="onClick" :class="classes">
 		<slot></slot>
 	</div>
 </template>
@@ -26,7 +26,8 @@
     computed: {
       classes() {
         return {
-          active: this.active
+          active: this.active,
+          disabled: this.disabled
         }
       }
     },
@@ -36,7 +37,8 @@
       })
     },
     methods: {
-      xxx() {
+      onClick() {
+        if (this.disabled) {return}
         this.eventBus.$emit('update:selected', this.name, this)
       }
     }
@@ -44,6 +46,7 @@
 </script>
 <style lang="scss" scoped>
 	$blue: blue;
+	$disabled-text-color: grey;
 	.tabs-item {
 		/*flex-grow: 1;*/
 		flex-shrink: 0;
@@ -57,5 +60,10 @@
 			color: $blue;
 			font-weight: bold;
 			}
+		
+		&.disabled {
+			color: $disabled-text-color;
+			}
+			
 		}
 </style>
