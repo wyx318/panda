ac7c81002baa697af601132d31963e34ccb9b5f9
wangyuixang <651033260@qq.com>
2019-11-15 00:26:26 +0800

    有一个 测试用例 暂时不通过

--- test/popover.test.js
+++ test/popover.test.js
@@ -11,7 +11,7 @@ describe('Popover', () => {
   })
   it('可以设置position', (done) => {
     Vue.component('g-popover', Popover);
-    const div = document.body.appendChild(div);
+    const div = document.createElement('div');
     document.body.appendChild(div);
     div.innerHTML = `
     <g-popover position="bottom" ref="a">
@@ -24,12 +24,11 @@ describe('Popover', () => {
     const vm = new Vue({
       el: div
     });
+
+    vm.$el.querySelector('button').click();
     vm.$nextTick(() => {
-      vm.$el.querySelector('button').click();
-      vm.$nextTick(() => {
-        const {contentWrapper} = vm.$refs.a.$refs
-        expect(contentWrapper.classList.contains('position-bottom')).to.be._true
-      })
+      const {contentWrapper} = vm.$refs.a.$refs;
+      expect(contentWrapper.classList.contains('position-bottom')).to.be.true;
       done()
     })
   })
